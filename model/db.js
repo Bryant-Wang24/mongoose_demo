0967adf6aee06a36990e2b6ea4f5d8d44ce478fa
Bryant-Wang24 <485969746@qq.com>
2021-10-10 15:56:37 +0800

    提示数据库连接成功

--- model/db.js
+++ model/db.js
@@ -1,6 +1,11 @@
 // 1、引入mongoose
 const mongoose = require('mongoose');
 // 2、建立连接
-mongoose.connect('mongodb://127.0.0.1:27017/wqs');
+mongoose.connect('mongodb://127.0.0.1:27017/wqs', { useNewUrlParser: true }, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('数据库连接成功');
+});
 
 module.exports = mongoose
